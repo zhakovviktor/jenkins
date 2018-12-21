92aad642ee6e9307b480a3225ea267444cc6d4ff
Viktor Zhakov <viktorzhakov@Viktors-MacBook-Pro.local>
2018-12-21 18:17:48 +0200

    add build from git

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,10 +1,26 @@
 pipeline {
     agent any
 
+    tools {
+        maven "M3"
+    }
+
+    parameters {
+        booleanParam defaultValue: true, description: '', name: 'skip_param'
+        string defaultValue: 'tag/jenkins-2.153', description: '', name: 'branch', trim: true
+    }
+
     stages {
+        stage ('Info'){
+            steps {
+                sh 'echo "skip_param = ${skip_param}"'
+                sh 'echo "branch = ${branch}"'
+            }
+
+        }
         stage ('Build'){
             steps {
-                sh 'echo "hello!"'
+                git branch: "${branch}", url: "https://github.com/jenkinsci/jenkins.git"
             }
         }
     }   
